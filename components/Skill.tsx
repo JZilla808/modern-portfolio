220bd6b3d2b1226709167cfce03670cbefade9e2
Jay Zhou <jzilla808@gmail.com>
2024-02-29 03:15:03 -0800

    Refactor Skill and Skills components

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-type Props = {
+interface SkillProps {
+  name: string;
+  iconPath: string;
+  proficiency: string;
   directionDown?: boolean;
-};
+}
 
-function Skill({ directionDown }: Props) {
+function Skill({
+  name,
+  iconPath,
+  proficiency,
+  directionDown = false,
+}: SkillProps) {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
         initial={{ y: directionDown ? -200 : 200, opacity: 0 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
-        src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png"
+        src={iconPath}
+        alt={name}
         className="rounded-full border border-gray-500 object-cover s8:w-16 s8:h-16 12pro:w-20 12pro:h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-200 ease-in-out"
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-200 ease-in-out group-hover:bg-white s8:w-16 s8:h-16 12pro:w-20 12pro:h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 ">
         <div className="flex items-center justify-center h-full">
-          <p className="s8:text-xl 12pro:text-2xl sm:text-3xl font-bold text-black opacity-100 ">
-            100%
+          <p className="s8:text-sm 12pro:text-sm sm:text-xl md:text-2xl font-bold text-black opacity-100 text-center ">
+            {name}
           </p>
         </div>
       </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,6 +4,104 @@ import Skill from "./Skill";
 
 type Props = {};
 // TODO: Add a list of skills, including the skill name, icon path and proficiency level
+const skillList = [
+  {
+    name: "JavaScript",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "90%",
+  },
+  {
+    name: "HTML",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "90%",
+  },
+  {
+    name: "CSS",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "90%",
+  },
+  {
+    name: "React",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "85%",
+  },
+  {
+    name: "TypeScript",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "85%",
+  },
+  {
+    name: "Angular",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "75%",
+  },
+  {
+    name: "Vue",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "85%",
+  },
+  {
+    name: "Git",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "90%",
+  },
+  {
+    name: "Node.js",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "70%",
+  },
+  {
+    name: "Express.js",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "70%",
+  },
+  {
+    name: "React Native",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "65%",
+  },
+  {
+    name: "MongoDB",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "60%",
+  },
+  {
+    name: "Python",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "70%",
+  },
+  {
+    name: "Go",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "60%",
+  },
+  {
+    name: "Rust",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "60%",
+  },
+  {
+    name: "Solidity",
+    iconPath:
+      "https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_96102ac6497377cd53da621075fe828e/sanity.png",
+    proficiency: "50%",
+  },
+];
 
 function Skills({}: Props) {
   return (
@@ -20,7 +118,18 @@ function Skills({}: Props) {
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
         Hover over a skill for current proficiency
       </h3>
-      <div className="grid grid-cols-4 gap-5 ">
+
+      <div className="grid grid-cols-4 gap-5">
+        {skillList.map((skill, index) => (
+          <Skill
+            key={index}
+            name={skill.name}
+            iconPath={skill.iconPath}
+            proficiency={skill.proficiency}
+          />
+        ))}
+
+        {/* <Skill />
         <Skill />
         <Skill />
         <Skill />
@@ -33,8 +142,7 @@ function Skills({}: Props) {
         <Skill />
         <Skill />
         <Skill />
-        <Skill />
-        <Skill />
+        <Skill /> */}
       </div>
     </motion.div>
   );
